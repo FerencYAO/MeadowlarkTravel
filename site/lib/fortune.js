7ece8d7a43d1ec1ae5da30755b2dda6c1989612b
yaolmh <yaolmh@126.com>
2019-07-08 10:26:32 +0800

    Moved 'fortune cookie' functionality into module.

---
+++ site/lib/fortune.js
@@ -0,0 +1,11 @@
+var fortuneCookies = [
+	"Conquer your fears or they will conquer you.",
+	"River need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple.",
+];
+exports.getFortune = function(){
+	var idx = Math.floor(Math.random()*fortunes.length);
+	return fortuneCookies[idx];
+}
--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"River need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
+var fortune = require('./lib/fortune.js');
+
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 app.set('port', process.env.PORT || 3000);
@@ -15,8 +10,7 @@ app.get('/',function(req,res){
 	res.render('home');
 });
 app.get('/about',function(req,res){
-	var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-	res.render('about', {fortune:randomFortune});
+	res.render('about', {fortune:fortune.getFortune()});
 });
 app.use(express.static(__dirname+'/public'));
 app.use(function(req, res, next){
