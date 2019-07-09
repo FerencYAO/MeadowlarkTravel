6b996e103e168ae4e0b44f032c83d65fbe623fdc
yaolmh <yaolmh@126.com>
2019-07-09 08:37:46 +0800

    add test code

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -6,13 +6,31 @@ var fortune = require('./lib/fortune.js');
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname+'/public'));
+app.use(function(req, res, next){
+	res.locals.showTests=app.get('env')!=='production' &&
+		req.query.test === '1';
+	next();
+});
+
 app.get('/',function(req,res){
 	res.render('home');
 });
 app.get('/about',function(req,res){
-	res.render('about', {fortune:fortune.getFortune()});
+	res.render('about', {
+		fortune:fortune.getFortune(),
+		pageTestScript:'/qa/tests-about.js'
+	});
 });
-app.use(express.static(__dirname+'/public'));
+app.get('/tours/hood-river',function(req,res){
+	res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate',function(req,res){
+	res.render('tours/request-group-rate');
+});
+
+
 app.use(function(req, res, next){
 	res.status(404);
 	res.render('404');
