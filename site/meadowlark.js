579a19ff2a23fda132c9d121de0e78a4d472e86f
yaolmh <yaolmh@126.com>
2019-07-09 17:55:17 +0800

    add weather data to home

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -2,15 +2,49 @@ var express = require('express');
 var app = express();
 var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
 var fortune = require('./lib/fortune.js');
+function getWeatherData(){
+	return {
+		locations:[
+		{
+			name:'Portland',
+			forecastUrl:'http://www.wunderground.com/US/OR/Portland.html',
+			iconUrl:'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+			weather:'Overcast',
+			temp:'54.1 F (12.3 C)',
+		},
+		{
+			name:'Bend',
+			forecastUrl:'http://www.wunderground.com/US/OR/Bend.html',
+			iconUrl:'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+			weather:'Partly Cloudy',
+			temp:'55.0 F (12.8 C)',
+		},
+		{
+			name:'Manzanita',
+			forecastUrl:'http://www.wunderground.com/US/OR/Manzanita.html',
+			iconUrl:'http://icons-ak.wxug.com/i/c/k/rain.gif',
+			weather:'Light Rain',
+			temp:'55.0 F (12.8 C)',
+		},
+		]
+	}
+};
+		
 
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 app.set('port', process.env.PORT || 3000);
+app.disable('x-powered-by');
 
 app.use(express.static(__dirname+'/public'));
 app.use(function(req, res, next){
 	res.locals.showTests=app.get('env')!=='production' &&
 		req.query.test === '1';
+	next();
+});
+app.use(function(req,res,next){
+	if(!res.locals.partials)res.locals.partials={};
+	res.locals.partials.weather = getWeatherData();
 	next();
 });
 
