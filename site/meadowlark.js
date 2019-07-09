311e1b30c94cf10a9aa0fa00209d8a8167aaf7c2
yaolmh <yaolmh@126.com>
2019-07-09 13:01:21 +0800

    add header display url path

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -29,6 +29,12 @@ app.get('/tours/hood-river',function(req,res){
 app.get('/tours/request-group-rate',function(req,res){
 	res.render('tours/request-group-rate');
 });
+/* app.get('/headers', function(req,res){
+	res.set('Content-Type','text/plain');
+	var s = '';
+	for (var name in req.headers) s+= name + ': ' + req.headers[name] + '\n';
+	res.send(s);
+}); */
 
 
 app.use(function(req, res, next){
