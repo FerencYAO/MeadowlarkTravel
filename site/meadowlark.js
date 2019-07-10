12e190f746dc6069380e2ed8339eb52320259834
yaolmh <yaolmh@126.com>
2019-07-10 17:56:48 +0800

    add ajax form

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -89,12 +89,22 @@ app.get('/data/nursery-rhyme',function(req,res){
 app.get('/newsletter',function(req,res){
 	res.render('newsletter',{csrf:'CSRF token goes here'});
 });
+app.get('/newsletter2',function(req,res){
+	res.render('newsletter2',{csrf:'CSRF token goes here'});
+});
 app.post('/process',function(req,res){
 	console.log('Form (from querystring): '+req.query.form);
 	console.log('CSRF token (from hidden form field): '+req.body._csrf);
 	console.log('Name (from visible form field): '+req.body.name);
 	console.log('Email (from visible form field): '+req.body.email);
 	res.redirect(303, '/thank-you');
+});
+app.post('/process2',function(req,res){
+	if(req.xhr || req.accepts('json,html')==='json'){
+		res.send({success:true});
+	}else{
+		res.redirect(303,'/thank-you');
+	}
 });
 /* app.get('/headers', function(req,res){
 	res.set('Content-Type','text/plain');
