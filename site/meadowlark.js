c0468c3348d7c974075982b480cca30ae4e62fff
yaolmh <yaolmh@126.com>
2019-07-11 17:04:32 +0800

    add cookie and session

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -12,7 +12,7 @@ var handlebars = require('express3-handlebars').create({defaultLayout:'main',hel
 var fortune = require('./lib/fortune.js');
 var formidable = require('formidable');
 var jqupload = require('jquery-file-upload-middleware');
-
+var credentials = require('./credentials.js');
 
 function getWeatherData(){
 	return {
@@ -71,6 +71,17 @@ app.use('/upload',function(req,res,next){
 		},
 	})(req,res,next);
 });
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+	resave:false,
+	saveUninitialized:false,
+	secret:credentials.cookieSecret,
+}));
+app.use(function(req,res,next){
+	res.locals.flash = req.session.flash;
+	delete req.session.flash;
+	next();
+});
 
 app.get('/',function(req,res){
 	res.render('home');
@@ -90,6 +101,13 @@ app.get('/tours/request-group-rate',function(req,res){
 app.get('/jquery-test', function(req,res){
 	res.render('jquery-test');
 });
+/* app.get('/headers', function(req,res){
+	res.set('Content-Type','text/plain');
+	var s = '';
+	for (var name in req.headers) s+= name + ': ' + req.headers[name] + '\n';
+	res.send(s);
+});
+ */
 app.get('/nursery-rhyme',function(req,res){
 	res.render('nursery-rhyme');
 });
@@ -113,6 +131,9 @@ app.get('/newsletter2',function(req,res){
 app.get('/contest/vacation-photo',function(req,res){
 	var now = new Date();
 	res.render('contest/vacation-photo',{year:now.getFullYear(),month:now.getMonth()});
+});
+app.get('/newsletter/archive',function(req,res){
+	res.render('newsletter/archive');
 });
 app.post('/process',function(req,res){
 	console.log('Form (from querystring): '+req.query.form);
@@ -139,13 +160,41 @@ app.post('/contest/vacation-photo/:year/:month',function(req,res){
 		res.redirect(303,'/thank-you');
 	});
 });
-/* app.get('/headers', function(req,res){
-	res.set('Content-Type','text/plain');
-	var s = '';
-	for (var name in req.headers) s+= name + ': ' + req.headers[name] + '\n';
-	res.send(s);
+function NewsletterSignup(){}
+NewsletterSignup.prototype.save = function(cb){
+	cb();
+};
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9,!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9]{0,61}[a-zA-Z0-9])?(?:\.[zA-Z0-9](?:[zA-Z0-9]{0,61}[a-zA-Z0-9])?)+$/;
+app.post('/newsletter',function(req,res){
+	var name=req.body.name||'',email=req.body.email||'';
+	if(!email.match(VALID_EMAIL_REGEX)){
+		if(req.xhr) return res.json({error:'Invalid name email address.'});
+		req.session.flash={
+			type:'danger',
+			intro:'Validation error!',
+			message:'The email address you entered was not valid.',
+		};
+		return res.redirect(303,'/newsletter/archive');
+	}
+	new NewsletterSignup({name:name,email:email}).save(function(err){
+		if(err){
+			if(req.xhr) return res.json({error:'Database error.'});
+			req.session.flash={
+				type:'danger',
+				intro:'Validation error!',
+				message:'The email address you entered was not valid.',
+			};
+			return res.redirect(303,'/newsletter/archive');
+		}
+		if(req.xhr) return res.json({success:true});
+		req.session.flash = {
+			type:'success',
+			intro:'Thank you!',
+			message:'You have now been signed up for the newsletter.',
+		};
+		return res.redirect(303,'/newsletter/archive');
+	});
 });
- */
 
 app.use(function(req, res, next){
 	res.status(404);
@@ -156,6 +205,7 @@ app.use(function(err,req,res,next){
 	res.status(500);
 	res.render('500');
 });
+
 app.listen(app.get('port'),function(){
 	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
