b1595da40e60dede4b53eea8e20830d77b2eef5a
yaolmh <yaolmh@126.com>
2019-07-31 15:27:48 +0800

    add attraction

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -323,6 +323,52 @@ app.get('/adventures/:subcat/:name',function(req,res,next){
 
 require('./routes.js')(app);
 
+app.use('/api', require('cors')());
+var Attraction = require('./models/attraction.js');
+app.get('/api/attractions',function(req,res){
+	Attraction.find({approved:true},function(err,attractions){
+		if(err) return res.send(500, 'Error occurred: database errror.');
+		res.json(attractions.map(function(a){
+			return {
+				name:a.name,
+				id:a._id,
+				description:a.description,
+				location:a.location,
+			}
+		}));
+	});
+});
+app.post('/api/attraction',function(req,res){
+	var a = new Attraction({
+		name:req.body.name,
+		description:req.body.description,
+		location:{lat:req.body.lat,lng:req.body.lng},
+		history:{
+			event:'created',
+			email:req.body.email,
+			date:new Date(),
+		},
+		approved:false,
+	});
+	a.save(function(err,a){
+		if(err) return res.send(500, 'Error occurred: database error.');
+		res.json({id:a._id});
+	});
+});
+app.get('/api/attraction/:id',function(req,res){
+	Attraction.findById(req.params.id,function(err, a){
+		if(err) return res.send(500, 'Error occurred: database error.');
+		res.json({
+			name:a.name,
+			id:a._id,
+			description:a.description,
+			location:a.location,
+		});
+	});
+});
+
+
+
 app.use(function(req, res, next){
 	res.status(404);
 	res.render('404');
